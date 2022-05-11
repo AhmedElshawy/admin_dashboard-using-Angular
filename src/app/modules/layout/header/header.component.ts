1d313233b9d3d1c616c6ee89f916b3cda8a15b49
Ahmed <elshawy28@gmail.com>
2022-05-11 12:22:22 +0200

    Ui Enhancements

---
+++ src/app/modules/layout/header/header.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AccountService } from './../../account/account.service';
+import { IUser } from './../../models/user';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  @Output() toggleSidebarForMe: EventEmitter<any> = new EventEmitter();
+  currentUser$!:Observable<IUser>
+
+  constructor(private router: Router, private accountService:AccountService) {}
+
+  ngOnInit(): void {
+    this.currentUser$ = this.accountService.currentUser$
+  }
+
+  toggleSidebar() {
+    this.toggleSidebarForMe.emit();
+  }
+
+  logout()
+  {
+    this.accountService.logOut();
+  }
+
+}
